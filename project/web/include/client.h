3daacbda4d6c7653cb5a1a43f2ba2e57390cf64c
thelvv <alex13mv@gmail.com>
2022-05-26 01:45:26 +0300

    for review

---
+++ project/web/include/client.h
@@ -0,0 +1,16 @@
+#ifndef PROJECT_WEB_INCLUDE_CLIENT_H_
+#define PROJECT_WEB_INCLUDE_CLIENT_H_
+
+#include <string>
+
+#include "client_tools.h"
+
+
+namespace client {
+    void sendMessage(const std::string &ip, const std::string &port, const std::string &path,
+                     const Message &message) {
+        client_tools::send(ip, port, path, message);
+    }
+}
+
+#endif  // PROJECT_WEB_INCLUDE_CLIENT_H_
