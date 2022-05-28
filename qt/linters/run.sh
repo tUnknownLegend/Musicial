e47892e2612c5300091e5f89d74bc2d5e2a0521f
Aint <pinevichv@gmail.com>
2022-05-28 03:37:01 +0300

    ci

--- qt/linters/run.sh
+++ qt/linters/run.sh
@@ -24,13 +24,13 @@ function check_log() {
 print_header "RUN cppcheck"
 check_log "cppcheck project --enable=warning --inconclusive --error-exitcode=0 -I ../* --suppress=missingIncludeSystem" "\(information\)"
 
-print_header "RUN clang-tidy"
-check_log "clang-tidy ../*.cpp -header-filter=.* -extra-arg=-std=c++11" "Error (?:reading|while processing)"
+#print_header "RUN clang-tidy"
+#check_log "clang-tidy ../*.cpp -header-filter=.* -extra-arg=-std=c++11" "Error (?:reading|while processing)"
 
 #print_header "RUN cppcheck"
 #check_log "cppcheck --verbose  --error-exitcode=0 * lib/*"
 
 #print_header "RUN cpplint"
-#check_log "cpplint --extensions=cpp, ../*.cpp * ../*.h" "Can't open for reading"
+#bash cpplint --extensions=cpp, ../*.cpp * ../*.h" "Can't open for reading"
 
 print_header "SUCCESS"
