233b4ba2945aa0d8a1c0eb05af494cf6902f14f5
Aint <pinevichv@gmail.com>
2022-05-28 04:07:31 +0300

    ci

---
+++ qt/lib/build/CMakeFiles/Musicial_test.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Musicial_test.dir/mainlib.cpp.o"
+  "CMakeFiles/Musicial_test.dir/mainlib.cpp.o.d"
+  "CMakeFiles/Musicial_test.dir/shared.cpp.o"
+  "CMakeFiles/Musicial_test.dir/shared.cpp.o.d"
+  "CMakeFiles/Musicial_test.dir/test.cpp.o"
+  "CMakeFiles/Musicial_test.dir/test.cpp.o.d"
+  "Musicial_test"
+  "Musicial_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Musicial_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
